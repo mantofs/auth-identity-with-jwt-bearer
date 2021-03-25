ac40a5e713c8c1424a80ed2320af01740fa44321
Marcos Silva <silvafmarcos@live.com>
2021-03-25 20:25:49 -0300

    Inclusão de migrations com dados iniciais

--- src/AuthIdentityWithJwtBearer.Application/Config/Settings.cs
+++ src/AuthIdentityWithJwtBearer.Application/Config/Settings.cs
@@ -7,5 +7,4 @@ namespace AuthIdentityWithJwtBearer.Config
     public string Audience { get; set; }
     public string TokenLifeTimeMinutes { get; set; }
   }
-  //"fedaf7d8863b48e197b9287d492b708e"
 }
